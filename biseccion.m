4894a43adf27cffc9eaf1ee4107dc123f8e0cf81
Fidencio162 <53537408+Fidencio162@users.noreply.github.com>
2020-09-17 23:10:39 -0500

    Create biseccion.m

---
+++ biseccion.m
@@ -0,0 +1,24 @@
+clear all
+format short;
+a=input('Introduzca el valor de a: ');
+b=input('Introduzca el valor de b: ');
+tol=input('Introduzca la tolerancia: ');
+fun=input('introduca la funcion f(x)=','s');
+f=inline(fun);
+k=1
+while((abs((b-a)/2)*100)>tol)
+    c=(a+b)/2;
+    if f(a)*f(c)<0
+        b=c;
+    else
+        a=c;
+    end
+    k=k+1;
+    e=abs((b-a)/2)*100;
+    A(k,:)=[k a b c f(c) e];
+end
+fprintf('\tk \ta \tb \tc \tf(c) \te \n')
+disp(A)
+fprintf('Solucion: \n raíz=%8.5f\n',c)
+fprintf('f(c)=%8.5f\n',f(c))
+fprintf('error=%8.5f\n',e)
