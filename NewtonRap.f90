870aacb4e0d07d13a083d6a41b192eda205efdb2
Fidencio162 <53537408+Fidencio162@users.noreply.github.com>
2020-09-24 23:48:49 -0500

    Create NewtonRap.f90

---
+++ NewtonRap.f90
@@ -0,0 +1,38 @@
+program NewtonRap
+  implicit double precision(a-h,o-z)
+	parameter(kmax=5000)
+	dimension r(kmax)
+	tol=2.0d0
+	! método de Newton-Rapson
+	! Condiciones Iniciales
+	k=0
+	r(0)=2
+	write(*,*)'------------------------------------------------------'
+	write(*,*)'|         k','  |       raíz','       |          error     |'
+	write(*,*)'------------------------------------------------------'
+	do
+	k=k+1
+	r(k+1)=r(k)-f(r(k))/df(r(k))
+	e=abs((r(k+1)-r(k))/r(k+1))*100.d0
+	c=r(k+1)
+	write(*,*) k,r(k+1),e
+	if (e.lt.tol) exit
+	enddo
+	write(*,*) '------------------------------------------------------'
+	write(*,*) 'Solucion:'
+	write(*,*) 'raíz=',c
+	write(*,*) 'error=',e
+   20	format(i5,2x,f3.8,2x,f3.8)
+end program
+
+	function f(x)
+	implicit double precision(a-h,o-z)
+	f=x**3-2.d0*x-5
+	return
+	end
+	
+	function df(x)
+	implicit double precision(a-h,o-z)
+	df=3.d0*x**2-2.d0
+	return
+	end
